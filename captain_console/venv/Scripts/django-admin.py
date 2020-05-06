dab9511f1fdc1e22444d5e2b10655a8b3f342bc3
andriberg19 <andri19@ru.is>
2020-05-06 13:23:45 +0000

    vonandi

---
+++ captain_console/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lenovo\Desktop\djangoproject\Verklegt2\captain_console\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
